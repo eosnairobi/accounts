88037117961f750c11de0815e9bedf29c58f9b4d
peterokwara <peter.okwara@outlook.com>
2018-08-09 18:38:21 +0300

    Generate keys and display on screen

--- src/components/keyGenerator/KeyGenerator.js
+++ src/components/keyGenerator/KeyGenerator.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
+import ecc from "eosjs-ecc";
+import ReactJson from "react-json-view";
 
 class KeyGenerator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { keys: [] };
+    this.generateNewKeys = this.generateNewKeys.bind(this);
+  }
+  generateNewKeys() {
+    const keys = this.state.keys.slice(0);
+    ecc.randomKey().then(privateKey => {
+      let publicKey = ecc.privateToPublic(privateKey);
+      keys.push([publicKey, privateKey]);
+      this.setState({
+        keys
+      });
+
+      console.log(this.state.publicKey);
+    });
+  }
   render() {
+    const { keys } = this.state;
+
     return (
       <div style={{ marginBottom: "13px" }}>
         <div class="btn-toolbar">
           <button
             type="button"
             class="btn btn-secondary btn-sm"
+            onClick={this.generateNewKeys}
             style={{ marginRight: "13px", marginTop: "10px" }}
           >
             {this.props.button1}
@@ -20,12 +42,17 @@ class KeyGenerator extends Component {
             {this.props.button2}
           </button>
         </div>
-        <textarea
-          class="form-control rounded-0"
-          id="exampleFormControlTextarea1"
-          rows="4"
-          style={{ marginTop: "13px" }}
-        />
+        <div>
+          <ReactJson
+            displayDataTypes={false}
+            displayObjectSize={false}
+            iconStyle="square"
+            name="keys"
+            src={keys}
+            style={{ padding: "1em", marginTop: "13px" }}
+            theme="harmonic"
+          />
+        </div>
       </div>
     );
   }
