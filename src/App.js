b682cd501902757f55b7e842c99b181d6abd78d0
peterokwara <peter.okwara@outlook.com>
2018-08-10 15:00:38 +0300

    Render home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import AccountForm from "./components/accountForm/AccountForm";
+import Home from "./components/home/Home";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AccountForm />
+        <Home />
       </div>
     );
   }
