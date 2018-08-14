463b70973c08cd26b5df25a8a943156c2a393ec1
peterokwara <peter.okwara@outlook.com>
2018-08-14 11:57:57 +0300

    Add mpesa payment form

---
+++ src/components/mpesaPayment/MpesaPayment.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MpesaPayment extends Component {
+  render() {
+    return (
+      <div class="mb-3" style={{ paddingTop: "20px" }}>
+        <label>Phone number</label>
+        <input class="form-control" />
+      </div>
+    );
+  }
+}
+
+export default MpesaPayment;
