31f60520dcdc5b7baadc269783843a29660f584e
peterokwara <peter.okwara@outlook.com>
2018-08-22 11:35:06 +0300

    Add states

--- src/components/accountDetails/AccountDetails.js
+++ src/components/accountDetails/AccountDetails.js
@@ -1,51 +1,48 @@
 import React, { Component } from "react";
+import InputForm from "../common/InputForm";
 
 class AccountForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { accountName: "", ownerPublicKey: "", activePublicKey: "" };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange = event => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
+  };
   render() {
+    console.log(this.state);
     return (
       <div>
-        <div class="form-group">
-          <label for="account-name">{this.props.labelOne}</label>
-          <input
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder={this.props.placeholderOne}
-          />
-          <small id="account-name" class="form-text text-muted">
-            a-z,1-5 are allowed only. Length 12
-          </small>
-        </div>
-        <div class="form-group">
-          <label for="owner-public-key">{this.props.labelTwo}</label>
-          <input
-            class="form-control"
-            id="owner-public-key"
-            placeholder={this.props.placeholderTwo}
-          />
-        </div>
-        <div class="form-group">
-          <label for="active-public-key">{this.props.labelThree}</label>
-          <input
-            class="form-control"
-            id="active-public-key"
-            placeholder={this.props.placeholderThree}
-          />
-        </div>
+        <InputForm
+          name="accountName"
+          label="Account Name"
+          placeholder="eosnairobike"
+          smallText="a-z,1-5 are allowed only. Length 12"
+          value={this.state.value}
+          onChange={this.handleInputChange}
+        />
+        <InputForm
+          name="ownerPublicKey"
+          label="Owner Public Key"
+          placeholder="EOS5wGBC13yRmxwgYUkRotn46a1B45BSnJFgNeqKhXS4jnPGqtNhZ"
+          value={this.state.value}
+          onChange={this.handleInputChange}
+        />
+        <InputForm
+          name="activePublicKey"
+          label="Active Public Key"
+          placeholder="EOS6LPGe3dv2HZnAD5d22rnFYwn1RZFYgyjKtgZyHWXVhEep82nDV"
+          value={this.state.value}
+          onChange={this.handleInputChange}
+        />
       </div>
     );
   }
 }
-
-AccountForm.defaultProps = {
-  header: "Create Account",
-  labelOne: "Account Name",
-  labelTwo: "Owner Public Key",
-  labelThree: "Active Public Key",
-  placeholderOne: "eosnairobike",
-  placeholderTwo: "EOS5wGBC13yRmxwgYUkRotn46a1B45BSnJFgNeqKhXS4jnPGqtNhZ",
-  placeholderThree: "EOS6LPGe3dv2HZnAD5d22rnFYwn1RZFYgyjKtgZyHWXVhEep82nDV",
-  buttonLabel: "Next"
-};
 
 export default AccountForm;
