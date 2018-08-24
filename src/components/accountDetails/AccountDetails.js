cf800255c9c5e62b304b574592e5868ef0f3bb98
peterokwara <peter.okwara@outlook.com>
2018-08-24 14:35:19 +0300

    Modal renders

--- src/components/accountDetails/AccountDetails.js
+++ src/components/accountDetails/AccountDetails.js
@@ -1,47 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import InputForm from "../common/InputForm";
 
-class AccountForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { accountName: "", ownerPublicKey: "", activePublicKey: "" };
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
+const AccountDetails = ({ handleInputChange, value }) => {
+  return (
+    <div>
+      <InputForm
+        name="accountName"
+        label="Account Name"
+        placeholder="eosnairobike"
+        smallText="a-z,1-5 are allowed only. Length 12"
+        value={value}
+        onChange={handleInputChange}
+      />
+      <InputForm
+        name="ownerPublicKey"
+        label="Owner Public Key"
+        placeholder="EOS5wGBC13yRmxwgYUkRotn46a1B45BSnJFgNeqKhXS4jnPGqtNhZ"
+        value={value}
+        onChange={handleInputChange}
+      />
+      <InputForm
+        name="activePublicKey"
+        label="Active Public Key"
+        placeholder="EOS6LPGe3dv2HZnAD5d22rnFYwn1RZFYgyjKtgZyHWXVhEep82nDV"
+        value={value}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
 
-  handleInputChange = event => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
-    this.setState({ [name]: value });
-  };
-  render() {
-    return (
-      <div>
-        <InputForm
-          name="accountName"
-          label="Account Name"
-          placeholder="eosnairobike"
-          smallText="a-z,1-5 are allowed only. Length 12"
-          value={this.state.value}
-          onChange={this.handleInputChange}
-        />
-        <InputForm
-          name="ownerPublicKey"
-          label="Owner Public Key"
-          placeholder="EOS5wGBC13yRmxwgYUkRotn46a1B45BSnJFgNeqKhXS4jnPGqtNhZ"
-          value={this.state.value}
-          onChange={this.handleInputChange}
-        />
-        <InputForm
-          name="activePublicKey"
-          label="Active Public Key"
-          placeholder="EOS6LPGe3dv2HZnAD5d22rnFYwn1RZFYgyjKtgZyHWXVhEep82nDV"
-          value={this.state.value}
-          onChange={this.handleInputChange}
-        />
-      </div>
-    );
-  }
-}
-
-export default AccountForm;
+export default AccountDetails;
