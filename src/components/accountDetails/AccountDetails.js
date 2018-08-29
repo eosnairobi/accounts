388d5d42518fd8021d3360291674f0537f6570c7
peterokwara <peter.okwara@outlook.com>
2018-08-29 15:10:14 +0300

    Change to props

--- src/components/accountDetails/AccountDetails.js
+++ src/components/accountDetails/AccountDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import InputForm from "../common/InputForm";
 
-const AccountDetails = ({ handleInputChange, value }) => {
+const AccountDetails = props => {
   return (
     <div>
       <InputForm
@@ -9,22 +9,22 @@ const AccountDetails = ({ handleInputChange, value }) => {
         label="Account Name"
         placeholder="eosnairobike"
         smallText="a-z,1-5 are allowed only. Length 12"
-        value={value}
-        onChange={handleInputChange}
+        value={props.value.accountName}
+        onChange={props.handleInputChange}
       />
       <InputForm
         name="ownerPublicKey"
         label="Owner Public Key"
         placeholder="EOS5wGBC13yRmxwgYUkRotn46a1B45BSnJFgNeqKhXS4jnPGqtNhZ"
-        value={value}
-        onChange={handleInputChange}
+        value={props.value.ownerPublicKey}
+        onChange={props.handleInputChange}
       />
       <InputForm
         name="activePublicKey"
         label="Active Public Key"
         placeholder="EOS6LPGe3dv2HZnAD5d22rnFYwn1RZFYgyjKtgZyHWXVhEep82nDV"
-        value={value}
-        onChange={handleInputChange}
+        value={props.value.activePublicKey}
+        onChange={props.handleInputChange}
       />
     </div>
   );
