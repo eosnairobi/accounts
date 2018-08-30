bdf5e377086f9ea3f6ada9bc2155021c35bee7af
peterokwara <peter.okwara@outlook.com>
2018-08-30 18:11:11 +0300

    Add routing paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
-import Home from "./container/home/Home";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
+import StartPage from "./container/startPage/StartPage";
+import GeneralInfoPage from "./container/generalPage/GeneralPage";
+import AccountPage from "./container/accountForm/AccountForm";
+import CheckoutPage from "./container/checkoutForm/CheckoutForm";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Switch>
+          <Route path="/" exact component={StartPage} />
+          <Route path="/general-info" component={GeneralInfoPage} />
+          <Route path="/account-info" component={AccountPage} />
+          <Route path="/checkout" component={CheckoutPage} />
+        </Switch>
       </div>
     );
   }
